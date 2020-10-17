ba3943a201c9138c6fb7766861a757f1ddb4d17b
Kabulzhan <kabulzhan@mail.ru>
2020-10-17 23:25:03 +0600

    Updates

---
+++ precache-manifest.15cebeaec83223bdf89c64b161b50d05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed29e672f02ab5c88a51d7a34e44245",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d286ed4ad91013956355",
+    "url": "/monsters-rolodex/static/css/main.0f521386.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d286ed4ad91013956355",
+    "url": "/monsters-rolodex/static/js/main.25e72e86.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
